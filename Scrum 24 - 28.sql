3fcdd64e3d874d86728907c81348dc5a9adca5e2
eldeivith <eldeivithxd@gmail.com>
2026-08-26 23:28:41 -0400

    Actualizacion del archivo
    Eliminacion de codigo repetido y mejora en comentarios

--- Scrum 24 - 28.sql
+++ Scrum 24 - 28.sql
@@ -1,4 +1,5 @@
 --- SCRUM - 24 Ejecución de Consultas DDL Básicas---
+
 CREATE TABLE auditoria_inventario (
     id_auditoria SERIAL PRIMARY KEY,
     fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -7,7 +8,9 @@ CREATE TABLE auditoria_inventario (
 ALTER TABLE auditoria_inventario ADD COLUMN usuario_responsable VARCHAR(100);
 ALTER TABLE auditoria_inventario DROP COLUMN usuario_responsable;
 DROP TABLE auditoria_inventario;
+
 --- SCRUM - 25 Operaciones DML y Mantenimiento de Datos---
+
 BEGIN; --En caso de fallas (solo lo probe en cmd)
 
 --Apartado 'a.1'
@@ -125,9 +128,8 @@ WHERE id_proveedor = 1;*/
 
 --En caso de fallas quiten los guiones del rollback
 --ROLLBACK; 
-Falta
---- SCRUM - 26 Construcción de Consultas Relacionales y Agrupaciones---
 
+--- SCRUM - 26 Construcción de Consultas Relacionales y Agrupaciones---
 
 1._Consulta de la lista de las órdenes junto al nombre del cliente
 SELECT 
@@ -206,101 +208,31 @@ ORDER BY p.nombre, pr.nombre_empresa;
 
 --- SCRUM 27 Gestión de Respaldos (Backup/Restore)---
 
-=======================================================================
-1._Respaldo base de datos (backup):
+--1._Respaldo base de datos (backup):
 -- ejecutar el comando en la terminal de linux (servidor)
 pg_dump -h "127.0.0.1" -U "ua_eq02" -d "ua_eq02" -F c -f respaldo_eq02.backup
-=======================================================================
-2._Crear nueva base de datos respaldada:
+
+--2._Crear nueva base de datos respaldada:
 -- ejecutar el comando dentro de la terminal psql
 CREATE DATABASE eq02_respaldado;
-=======================================================================
-3._Restaurar respaldo:
+
+--3._Restaurar respaldo:
 -- ejecutar el comando en la terminal de linux (servidor)
 pg_restore -h "127.0.0.1" -U "ua_eq02" -d "eq02_respaldado" -1 respaldo_eq02.backup
-=======================================================================
-4._Verificación final:
+
+--4._Verificación final:
 --ingresar a la nueva base de datos creada
 psql -h "127.0.0.1" -U "ua_eq02" -d "eq02_respaldado"
-=======================================================================
-5._Prueba en psql:
+
+--5._Prueba en psql:
 -- verificar tablas de la base de datos y hacer una consulta
 \dt
 SELECT * FROM Inventario LIMIT 5;
-
----SCRUM 25 Operaciones DML y Mantenimiento de Datos---
-
---Comando Insercion de un nuevo proveedor,
-INSERT INTO Clientes (rut, nombre, direccion, telefono)
-VALUES (
-    '76.111.161-1',
-    'Cliente Prueba SpA',
-    'Av. Prueba 100, Santiago',
-    '912345721'
-);
-
---Comando De verificacion
-SELECT *
-FROM Clientes
-WHERE rut = '76.111.161-1'; 
-============================================================
---Comando de Insercion de Producto
-INSERT INTO Productos (nombre, descripcion, precio, id_categoria)
-VALUES (
-    'Webcam Full HD',
-    'Cámara web Full HD para videollamadas',
-    29990,
-    2
-);
-
---Comando de verificacion
-SELECT *
-FROM Productos
-WHERE nombre = 'Webcam Full HD';
-============================================================
---Comando para actualizar stock
-UPDATE Inventario
-SET cantidad = cantidad + 10
-WHERE id_producto = 1
-  AND id_ubicacion = 1;
-
---Comando de verificacion
-SELECT 
-    i.id_inventario,
-    p.nombre AS producto,
-    i.id_ubicacion,
-    i.cantidad AS stock
-FROM Inventario i
-JOIN Productos p 
-    ON i.id_producto = p.id_producto
-WHERE i.id_producto = 1
-  AND i.id_ubicacion = 1; 
-============================================================
---Comando para actualizar contacto
-UPDATE Proveedores
-SET contacto = 'nuevo.contacto@tecnochile.cl'
-WHERE id_proveedor = 1;
-
---Comando de verificacion
-SELECT 
-    id_proveedor,
-    nombre_empresa,
-    contacto
-FROM Proveedores
-WHERE id_proveedor = 1;
-============================================================
---Comando para borrar un dato
-DELETE FROM Clientes
-WHERE rut = '76.111.161-1';
-
---Comando de verificacion
-SELECT *
-FROM Clientes
-WHERE rut = '76.111.161-1';
-
-=========================================================
  
-    --TRANSACCION 1 BEGIN + COMMIT
+--Consultas TCL 
+
+--TRANSACCION 1 BEGIN + COMMIT
+
 BEGIN;
 
 WITH nueva_orden AS (
